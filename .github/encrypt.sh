62e4f3317e97d412e67fc328ffbb9869f85b4824
tknr <geishagirlskoume@gmail.com>
2024-10-28 10:59:22 +0900

    github action

--- .github/encrypt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-openssl aes-256-cbc -pbkdf2 -e -in id_rsa -out ./id_rsa.cipher -k "${KEY}"
--- .github/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/sbin/my_init &
-chmod 600 /root/.ssh/id_rsa
-pwsh ./root/scoop/lib/install.ps1
-bash -x /root/excavate.sh
